590b47ed78609e892f0179d36c1cf4634649372b
vladimirmorev <v.morev90@gmail.com>
2015-12-06 18:08:50 +0600

    add authorization controller and refactor social oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,42 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :posts
   has_many :comments
+  has_many :authorizations
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :omniauthable, :omniauth_providers => [:twitter, :facebook, :linkedin]
  
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider 
-      user.uid      = auth.uid
-      user.name     = auth.info.name
-      user.save
+  SOCIALS = {
+    facebook: 'Facebook',
+    twitter: 'Twitter',
+    linkedin: 'Linkedin'
+  }  
+
+  def self.from_omniauth(auth, current_user)
+    authorization = Authorization.where(:provider => auth.provider, :uid => auth.uid.to_s, 
+                                        :token => auth.credentials.token, 
+                                        :secret => auth.credentials.secret).first_or_initialize
+    #authorization.profile_page = auth.info.urls.first.last unless authorization.persisted?
+    if authorization.user.blank?
+      user = current_user.nil? ? User.where('email = ?', auth['info']['email']).first : current_user
+      if user.blank?
+        user = User.new
+        #user.skip_confirmation!
+        user.password = Devise.friendly_token[0, 20]
+        user.fetch_details(auth)
+        user.save
+      end
+      authorization.user = user
+      authorization.save
     end
+    authorization.user
+  end
+
+  def fetch_details(auth)
+    self.name = auth.info.name
+    self.email = auth.info.email
+    #self.photo = URI.parse(auth.info.image)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204152400) do
+ActiveRecord::Schema.define(version: 20151206094646) do
+
+  create_table "authorizations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "token"
+    t.string   "secret"
+    t.string   "profile_page"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
