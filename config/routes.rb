5f9b52f6187ce6956e67ea085a21e7970b3b9cbd
vladimirmorev <v.morev90@gmail.com>
2015-12-19 12:46:16 +0600

    Create user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+  resources :users
   
   resources :posts do
      resources :comments
