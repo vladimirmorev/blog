2848549d8b85512aa5cec66a8a5eff93489127cc
vladimirmorev <v.morev90@gmail.com>
2015-12-19 13:52:29 +0600

    Add user index and user show pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
 
-	def index
-	  @users = User.all;
+	def index	  
+	  @users = User.paginate(page: params[:page], :per_page => 50) 
 	end
 
 	def show
-	  @user = User.find(params[:id])
+	  @user = User.find(params[:id])	  
 	end
+
+	def destroy
+      @user = User.find(params[:id])
+
+      if @user.destroy
+        redirect_to root_url, notice: "User deleted."
+      end
+    end
 end
