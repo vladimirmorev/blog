bf70b85b0d7d898c71ef176628c98cea8396320b
vladimirmorev <v.morev90@gmail.com>
2015-12-21 17:04:47 +0600

    modify users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-  resources :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }  
+
+  get 'users/:id' => 'users#show'
   
   resources :posts do
      resources :comments
