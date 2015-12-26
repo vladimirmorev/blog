4706707bd460e9b42d2863d3508e1cf102b6a5ea
vladimirmorev <v.morev90@gmail.com>
2015-12-26 15:22:43 +0600

    some test post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Post saves with all parameters" do
+    post = Post.create(title: 'Post title', body: 'Test')
+    assert post.valid?, 'The post was not valid when all parameters were supplied' 
+    assert_equal 'Post title', post.title, 'Post title does not match'
+  end
+
+  test "Post doesn't save without all parameters" do
+    post = Post.create(title: 'Post title')
+    assert_not post.valid?, 'The post should not be valid when missing body'
+  end
+  
 end
