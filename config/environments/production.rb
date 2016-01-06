fc49aa0c97e1793038e036698bf83ef629050787
vladimirmorev <v.morev90@gmail.com>
2016-01-06 14:03:14 +0600

    change addres in smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.sendgrid.net",
     port: 587,
     domain: ENV["GMAIL_DOMAIN"],
     authentication: "plain",
