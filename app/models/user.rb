40f5514cc9b2bc557e4943c16cd69d8dd5d171e4
vladimirmorev <v.morev90@gmail.com>
2016-01-07 12:04:33 +0600

    modify name parsing in order to fit vkontakte hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,7 +42,7 @@ class User < ActiveRecord::Base
       # Create the user if it's a new registration
       if user.nil?
         user = User.new(
-          name: auth.extra.raw_info.name || auth.info.nickname || auth.uid,
+          name: auth.extra.raw_info.name || auth.info.name || auth.info.nickname || auth.uid,
           #username: auth.info.nickname || auth.uid,
           email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
           password: Devise.friendly_token[0,20]
