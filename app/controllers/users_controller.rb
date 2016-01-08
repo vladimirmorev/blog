81051c7403d2be92e3e2b46ab3edc3523e3c7bb9
vladimirmorev <v.morev90@gmail.com>
2016-01-08 19:03:52 +0600

    refactoring 'Replace instance variable with local variable'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,10 +43,7 @@ class UsersController < ApplicationController
     end
   end
   
-  private
-    def set_user
-      @user = User.find(params[:id])
-    end
+  private   
 
     def user_params
       accessible = [ :name, :email ] # extend with your own params
