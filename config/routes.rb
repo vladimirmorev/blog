66632ce33ad4d35ddd588f152b06eed10e598186
vladimirmorev <v.morev90@gmail.com>
2016-01-09 20:13:34 +0600

    restrict auto-generated routes posts/comments (only: [:create, :destroy])

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
   resources :posts do
-     resources :comments
+     resources :comments, only: [:create, :destroy]
   end
 
   root 'posts#index'
