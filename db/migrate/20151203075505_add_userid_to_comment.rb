a0cd68a5ef60f26de13abaf4c5cc23f1891df1ac
vladimirmorev <v.morev90@gmail.com>
2016-01-11 15:32:42 +0600

    fix issue with removing commontator

--- db/migrate/20151203075505_add_userid_to_comment.rb
+++
@@ -1,5 +0,0 @@
-class AddUseridToComment < ActiveRecord::Migration
-  def change
-    add_reference(:comments, :user, foreign_key: true, index: true)
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -13,17 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151210044929) do
 
-  create_table "comments", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer  "user_id"
-  end
-
-  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
-
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "provider"
