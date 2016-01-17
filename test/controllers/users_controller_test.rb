9aefa0f320749f27fe9ce696137e503559d18b8e
vladimirmorev <v.morev90@gmail.com>
2016-01-17 10:40:54 +0600

    add test users_controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::TestHelpers                   
+  include Warden::Test::Helpers	
+  
+  setup do    
+    @user = users(:tom)
+    @user.confirm
+    sign_in @user       
+  end
+
+  test "should show user" do
+    get :show, id: @user
+    assert_response :success
+  end
+
 end
